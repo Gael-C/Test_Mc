4a4661efa73ecf0372392335e3a0e2219d552357
Gaël La Rochelle <gael_coupe17@outlook.fr>
2022-07-05 10:50:13 +0200

    Exercice 3 création d'un endpoint pour ajout d'une écriture dans un compte

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,14 +2,17 @@
 
 	namespace App\Controller;
 
+	use App\Entity\Ecritures;
 	use App\Repository\EcrituresRepository;
 	use Doctrine\DBAL\Exception;
+	use Doctrine\ORM\EntityManagerInterface;
+	use Ramsey\Uuid\Uuid;
 	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Routing\Annotation\Route;
 	use Symfony\Component\Serializer\Exception\ExceptionInterface;
-	use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+	use Symfony\Component\Serializer\SerializerInterface;
 
 	class ApiController extends AbstractController
 	{
@@ -17,34 +20,72 @@
 		 * @Route("/api/comptes/{uuid}/ecritures", name="api_post_index",methods={"GET"})
 		 * @throws Exception|ExceptionInterface
 		 */
-		public function index(EcrituresRepository $repo, NormalizerInterface  $norma, Request $request)
+		public function index(EcrituresRepository $repo, Request $request)
 		{
 			$uuid = $request->attributes->get('uuid');
 
 			$ecritures = $repo->findEcritures($uuid);
 
-			if (!empty($ecritures)){
-				$ecrituresNormalises = $norma->normalize($ecritures);
+			if (!empty($ecritures)) {
 
-				$json = json_encode($ecrituresNormalises);
+				return $this->json($ecritures, 200, []);
 
-				return new Response($json,200,[
-					"Content-Type"=>"application/json"
-				]);
-			}else{
-				return new Response('Aucunes écritures trouves avec cet uuid',404);
+			} else {
+
+				return new Response('Aucunes écritures trouves avec cet uuid', 404);
+
 			}
-
 		}
 
 		/**
-		 * @Route("/api/post", name="api_post_Post", methods={"POST"})
-         */
-		public function create(Request $request)
-	 	{
-			 $jsonRecu = $request->getContent();
+		 * @Route("/api/comptes/{uuid}/ecritures", name="api_post_Post", methods={"POST"})
+		 * @throws Exception
+		 * @throws Ramsey
+		 */
+		public function create(Request $request, SerializerInterface $seria, EntityManagerInterface $em)
+		{
+			$t_d = date_create('');
+			$today_date = $t_d->format('m.d.Y');
+			$c_uuid = $request->attributes->get('uuid');
 
-			 dd($jsonRecu);
+			if (!empty($c_uuid))
+			{
+				$jsonRecu = $request->getContent();
 
-	 	}
-}
+				$ecritures = $seria->deserialize($jsonRecu, Ecritures::class, 'json');
+
+				$uuid = $ecritures->setUuid(Uuid::uuid4())->__toString();
+				$label = $ecritures->getLabel();
+				$type = $ecritures->getType();
+				$amount = $ecritures->getAmount();
+				$d = $ecritures->getDate();
+				$date = $d->format('m.d.Y');
+
+				if ($date >= $today_date && $amount > 0)
+				{
+					$conn = $em->getConnection();
+
+					$stmt = $conn->prepare('INSERT INTO ecritures (uuid, compte_uuid, label, date, type, amount)
+			VALUES (:uuid, :c_uuid, :label,:date, :type, :amount)
+			');
+					$stmt->bindParam(':uuid', $uuid);
+					$stmt->bindParam(':c_uuid', $compte_uuid);
+					$stmt->bindParam(':label', $label);
+					$stmt->bindParam(':date', $date);
+					$stmt->bindParam(':type', $type);
+					$stmt->bindParam(':amount', $amount);
+
+					$stmt->executeStatement();
+
+					return new Response('Uuid créé avec succès', 201);
+
+				} else
+				{
+					return new Response('Date et/ou montant non valide, merci de renseigner une date supérieur à la date du jour et/ou un montant positif', 404);
+				}
+			}else
+			{
+				return new Response('Merci d\'entrer un numéro de compte valide', 404);
+			}
+		}
+	}
--- src/Entity/Ecritures.php
+++ src/Entity/Ecritures.php
@@ -26,182 +26,165 @@ class Ecritures
 	 * @ORM\CustomIdGenerator(class=UuidGenerator::class)
 	 * @Groups("post:read")
 	 */
-    private $uuid;
+	private $uuid;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="label", type="string", length=255, nullable=false)
-	 * @Groups("post:read")
-
-	 */
-    private $label;
-
-    /**
-     * @var DateTime|null
-     *
-     * @ORM\Column(name="date", type="date", nullable=true)
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="label", type="string", length=255, nullable=false)
 	 * @Groups("post:read")
 	 */
-    private $date;
+	private $label;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="type", type="string", length=0, nullable=false)
+	/**
+	 * @var DateTime|null
+	 *
+	 * @ORM\Column(name="date", type="date", nullable=true)
 	 * @Groups("post:read")
 	 */
-    private $type;
+	private $date;
 
-    /**
-     * @var float
-     *
-     * @ORM\Column(name="amount", type="float", precision=10, scale=0, nullable=false)
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="type", type="string", length=0, nullable=false)
 	 * @Groups("post:read")
 	 */
-    private $amount;
+	private $type;
 
-    /**
-     * @var DateTime|null
-     *
-     * @ORM\Column(name="created_at", type="datetime", nullable=true, options={"default"="CURRENT_TIMESTAMP"})
-     */
-    private $createdAt = 'CURRENT_TIMESTAMP';
+	/**
+	 * @var float
+	 *
+	 * @ORM\Column(name="amount", type="float", precision=10, scale=0, nullable=false)
+	 * @Groups("post:read")
+	 */
+	private $amount;
 
-    /**
-     * @var DateTime|null
-     *
-     * @ORM\Column(name="updated_at", type="datetime", nullable=true, options={"default"="CURRENT_TIMESTAMP"})
-     */
-    private $updatedAt = 'CURRENT_TIMESTAMP';
+	/**
+	 * @var DateTime|null
+	 *
+	 * @ORM\Column(name="created_at", type="datetime", nullable=true, options={"default"="CURRENT_TIMESTAMP"})
+	 */
+	private $createdAt = 'CURRENT_TIMESTAMP';
 
-    /**
-     * @var \Comptes
-     *
-     * @ORM\ManyToOne(targetEntity="Comptes")
-     * @ORM\JoinColumn(name="compte_uuid", referencedColumnName="uuid")
-     *
-     */
-    private $compteUuid;
+	/**
+	 * @var DateTime|null
+	 *
+	 * @ORM\Column(name="updated_at", type="datetime", nullable=true, options={"default"="CURRENT_TIMESTAMP"})
+	 */
+	private $updatedAt = 'CURRENT_TIMESTAMP';
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\ManyToOne(targetEntity="Comptes")
+	 * @ORM\JoinColumn(name="compte_uuid", referencedColumnName="uuid")
+	 *
+	 */
+	private $compteUuid;
+
+
+	public function getUuid()
+	{
+		return $this->uuid;
+	}
 
 	/**
 	 * @param Uuid $uuid
-	 * @param string $label
-	 * @param DateTime|null $date
-	 * @param string $type
-	 * @param float $amount
-	 * @param DateTime|string|null $createdAt
-	 * @param DateTime|string|null $updatedAt
-	 * @param \Comptes $compteUuid
 	 */
-	public function __construct(Uuid $uuid, string $label, ?DateTime $date, string $type, float $amount, $createdAt, $updatedAt, \Comptes $compteUuid)
-                                             	{
-                                             		$this->uuid = Uuid::uuid4();
-                                             		$this->label = $label;
-                                             		$this->date = $date;
-                                             		$this->type = $type;
-                                             		$this->amount = $amount;
-                                             		$this->createdAt = $createdAt;
-                                             		$this->updatedAt = $updatedAt;
-                                             		$this->compteUuid = $compteUuid;
-                                             	}
-
-    public function getUuid() : UuidInterface
+	public function setUuid(UuidInterface $uuid): self
 	{
-        return $this->uuid;
-    }
+		$this->uuid = $uuid->toString();
 
-	/**
-	 * @param UuidInterface $uuid
-	 */
-	public function setUuid(UuidInterface $uuid): void
-	{
-		$this->uuid = $uuid;
+		return $this;
 	}
 
-    public function getLabel(): ?string
-    {
-        return $this->label;
-    }
+	public function getLabel(): ?string
+	{
+		return $this->label;
+	}
 
-    public function setLabel(string $label): self
-    {
-        $this->label = $label;
+	public function setLabel(string $label): self
+	{
+		$this->label = $label;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getDate(): ?\DateTimeInterface
-    {
-        return $this->date;
-    }
+	public function getDate(): ?\DateTimeInterface
+	{
+		return $this->date;
+	}
 
-    public function setDate(?\DateTimeInterface $date): self
-    {
-        $this->date = $date;
+	public function setDate(?\DateTimeInterface $date): self
+	{
+		$this->date = $date;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getType(): ?string
-    {
-        return $this->type;
-    }
+	public function getType(): ?string
+	{
+		return $this->type;
+	}
 
-    public function setType(string $type): self
-    {
-        $this->type = $type;
+	public function setType(string $type): self
+	{
+		$this->type = $type;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getAmount(): ?float
-    {
-        return $this->amount;
-    }
+	public function getAmount(): ?float
+	{
+		return $this->amount;
+	}
 
-    public function setAmount(float $amount): self
-    {
-        $this->amount = $amount;
+	public function setAmount(float $amount): self
+	{
+		$this->amount = $amount;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
+	public function getCreatedAt(): ?\DateTimeInterface
+	{
+		return $this->createdAt;
+	}
 
-    public function setCreatedAt(?\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
+	public function setCreatedAt(?\DateTimeInterface $createdAt): self
+	{
+		$this->createdAt = $createdAt;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
+	public function getUpdatedAt(): ?\DateTimeInterface
+	{
+		return $this->updatedAt;
+	}
 
-    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
+	public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+	{
+		$this->updatedAt = $updatedAt;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getCompteUuid(): ?Comptes
-    {
-        return $this->compteUuid;
-    }
+	public function getCompteUuid(): ?string
+	{
+		return $this->compteUuid;
+	}
 
-    public function setCompteUuid(?Comptes $compteUuid): self
-    {
-        $this->compteUuid = $compteUuid;
+	public function setCompteUuid(?string $compteUuid): self
+	{
+		$this->compteUuid = $compteUuid;
 
-        return $this;
-    }
+		return $this;
+	}
 
-
+	public function __toString():string
+	{
+		return $this->uuid;
+	}
 }
